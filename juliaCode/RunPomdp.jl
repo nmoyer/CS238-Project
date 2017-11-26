afd7890d86f9c7403e4698d77458fcc0e7d79f21
Shushman Choudhury <shushmanchoudhury@gmail.com>
2017-11-26 13:51:12 -0800

    Reasonable lambdas

--- juliaCode/RunPomdp.jl
+++ juliaCode/RunPomdp.jl
@@ -10,10 +10,11 @@ GRID_SIZE = 10
 # Default parameters: map_size is 20 x 20; true_map is all cells true and true_battery_left is 100
 # TODO : Cost of NFZ should be HIGH to encourage sensor usage
 sensors = [LineSensor([0,1]),LineSensor([1,0]),LineSensor([0,-1]),LineSensor([-1,0]),CircularSensor()]
-lambdas = [0.0,0.0,10000.0]
+lambdas = [1.0,50.0,100.0]
 pomdp = UAVpomdp(GRID_SIZE, falses(GRID_SIZE,GRID_SIZE), [1,1], [GRID_SIZE,GRID_SIZE], sensors, lambdas)
 
 solver = POMCPOWSolver(tree_queries=1000)
+
 policy = solve(solver, pomdp);
 
 sim = SimulatorState(GRID_SIZE,100)
@@ -31,8 +32,7 @@ n = 1
 while true
     update_simulator(sim, state, belief_state)
     
-    a = action(policy, belief_state) 
-
+    a = action(policy, belief_state)
     new_state = generate_s(pomdp, state, a, rng)
     total_reward += reward(pomdp, state, a, new_state)
     obs = generate_o(pomdp, state, a, new_state, rng)
