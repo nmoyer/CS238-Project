3af3cafdae8d9927b65aea2cfc2b1f02f0e28da6
Shushman Choudhury <shushmanchoudhury@gmail.com>
2017-12-06 20:16:11 -0800

    Making it ready to run on ssh

--- juliaCode/RunPomdp.jl
+++ juliaCode/RunPomdp.jl
@@ -13,7 +13,7 @@ function run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_si
         initial_belief_map[START_LOC[1],START_LOC[2]] = 0.0
         state = MDPState(START_LOC, [START_BATTERY,eps()], initial_belief_map)
 
-    elseif alg == "pomdp"
+    elseif alg == "pomcp"
 
         const solver = POMCPSolver(tree_queries=TREE_QUERIES, c=C, max_depth=MAX_DEPTH, rng=rng)
         const pomdp = UAVpomdp(grid_size, initial_map, START_LOC, [grid_size, grid_size], sensors, lambdas)
@@ -35,6 +35,7 @@ function run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_si
 
     total_reward = 0
     iteration = 0
+    solved = false
     while iteration < 1000
         if !suppress_sim
             if alg == "mdp"
@@ -46,7 +47,7 @@ function run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_si
 
         if alg == "mdp"
             a = action(policy, state)
-        elseif alg == "pomdp"
+        elseif alg == "pomcp"
             a = action(policy, belief_state)
         else
             a = greedy_information_action(pomdp, belief_state)
@@ -66,12 +67,18 @@ function run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_si
 
         if alg == "mdp"
             if isterminal(mdp, state)
-                update_simulator(sim, state)
+                if !suppress_sim
+                    update_simulator(sim, state)
+                end
+                solved=true
                 break
             end
         else
-            if isterminal(pomdp, state) 
-                update_simulator(sim, state, belief_state)
+            if isterminal(pomdp, state)
+                if !suppress_sim
+                    update_simulator(sim, state, belief_state)
+                end
+                solved=true
                 break
             end
         end
@@ -79,7 +86,7 @@ function run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_si
         iteration += 1
     end
 
-    return total_reward 
+    return total_reward,solved
 end 
 
 
@@ -87,48 +94,56 @@ function run_trials(sim, sensors, lambdas, num_trials, suppress_sim, start_seed,
 
     oracle_cost = -100000000000.0
     initial_map = 0
+    free_path = 1
+    cp_start_seed = start_seed
 
-    print((SUCCESS_LAMBDA-MOVEMENT_LAMBDA*2*(grid_size-1)),oracle_cost)
-    print("\n")
-    while oracle_cost < (SUCCESS_LAMBDA-MOVEMENT_LAMBDA*2*(grid_size-1))
-        print(string(oracle_cost)*"\n")
-        start_seed += 1
-        rng = Base.Random.MersenneTwister(start_seed)
+    while free_path > 0
+        cp_start_seed += 1
+        rng = Base.Random.MersenneTwister(cp_start_seed)
         initial_map = initialize_map(grid_size, percent_obstruct, rng)
-        oracle_cost = cost_of_oracle(initial_map, START_LOC, [grid_size, grid_size], lambdas)
+        free_path, oracle_cost = cost_of_oracle(initial_map, START_LOC, [grid_size, grid_size], lambdas)
     end
 
     rewards = []
+    n_no_sols = 0
 
-    for seed in start_seed:start_seed + (num_trials-1)
+    for seed in cp_start_seed:cp_start_seed + (num_trials-1)
 
         rng = Base.Random.MersenneTwister(seed)
-        reward = run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_size, suppress_sim)
+        reward,solved = run_iteration_alg(sim, sensors, lambdas, rng, initial_map, alg, grid_size, suppress_sim)
         push!(rewards, reward)
+        if solved == false
+            n_no_sols +=1
+        end
     end
 
     avg_rewards = sum(rewards) ./ num_trials
     std_dev_rewards = (sum([(reward-avg_rewards)^2 for reward in rewards])/num_trials)^(0.5)
 
-    return avg_rewards, std_dev_rewards
+    return avg_rewards, std_dev_rewards, oracle_cost, n_no_sols
 end
 
 function run_trials_with_alg(sensors, lambdas, NUM_TRIALS, SUPPRESS_SIM, START_SEED, alg)
 
-for grid_size = 11#7:4:15
-    for percent_obstruct = 0.1:0.1:0.3
+    print("Trying with "*alg*"\n")
+    for grid_size = 10:5:20
+        for percent_obstruct = 0.1:0.1:0.3
 
-        if !SUPPRESS_SIM
-            sim = SimulatorState(grid_size,0)
-        else
-            sim = 0
+            if !SUPPRESS_SIM
+                sim = SimulatorState(grid_size,0)
+            else
+                sim = 0
+            end
+
+            print("***Grid size="*string(grid_size)*",percent_obstructed="*string(percent_obstruct)*"***\n")
+            avg_cost, std_dev_cost, oracle_cost, n_no_sols = run_trials(sim, sensors, lambdas, NUM_TRIALS, SUPPRESS_SIM, START_SEED, alg, grid_size, percent_obstruct)
+            print("Avg reward - "*string(avg_cost)"\n")
+            fname = alg*"-"*string(grid_size)*"-"*string(percent_obstruct)*".txt"
+            open(fname,"w") do f
+                write(f,"$avg_cost, $std_dev_cost, $oracle_cost $n_no_sols")
+            end
         end
-
-        print("***Grid size="*string(grid_size)*",percent_obstructed="*string(percent_obstruct)*"***\n")
-        avg_cost, std_dev_cost = run_trials(sim, sensors, lambdas, NUM_TRIALS, SUPPRESS_SIM, START_SEED, alg, grid_size, percent_obstruct)
-        print(avg_cost, std_dev_cost)
     end
-end
 
 end
 
@@ -145,10 +160,10 @@ const START_BATTERY = 0.0
 const MOVEMENT_LAMBDA = 1.0
 const HEURISTIC_LAMBDA = 1.0
 const SENSOR_LAMBDA = 1.0
-const NFZ_LAMBDA = 10
+const NFZ_LAMBDA = 20
 const SUCCESS_LAMBDA = 1000.0
 
-const SUPPRESS_SIM = false
+const SUPPRESS_SIM = true
 
 const sensors = [LineSensor([0,1]),LineSensor([1,0]),LineSensor([0,-1]),LineSensor([-1,0]),CircularSensor()]
 const lambdas = [MOVEMENT_LAMBDA, HEURISTIC_LAMBDA, SENSOR_LAMBDA, NFZ_LAMBDA, SUCCESS_LAMBDA]
@@ -160,7 +175,7 @@ const MAX_DEPTH = 40
 const NUM_TRIALS = 1
 const START_SEED = 300
 
-for algorithm in ["pomdp","pomdp"]
+for algorithm in ["mdp","pomcp","greedy"]
     run_trials_with_alg(sensors, lambdas, NUM_TRIALS, SUPPRESS_SIM, START_SEED, algorithm)
 end
 
